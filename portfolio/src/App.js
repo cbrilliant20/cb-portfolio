707f9ee6832718af910481d251ec9dc9ea656527
Cameron Brilliant <cbrilliant20@gmail.com>
2021-07-07 12:56:45 -0400

    remove boiler, added file structure, home component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css"
+import Home from "./screens/Home/Home"
 
 function App() {
   return (
     <div className="App">
-
+      <Home />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ portfolio/src/screens/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Hello, I'm Cameron Brilliant</h1>
+      <h2>Full-Stack Web Developer</h2>
+      <button className="welcome-btn btn">Welcome</button>
+    </div>
+  )
+}
+
+export default Home
