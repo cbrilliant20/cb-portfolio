c317e14cd0198368d1df2aa59f0c8746e6aa73c7
Cameron Brilliant <cbrilliant20@gmail.com>
2021-07-12 12:30:08 -0400

    about compnent 1/2 done

--- portfolio/src/screens/About/About.jsx
+++ portfolio/src/screens/About/About.jsx
@@ -2,8 +2,68 @@ import "./About.css"
 
 const About = () => {
   return (
-    <div>
+    <div className="about-container">
       <h2>About</h2>
+      <div className="skills-container">
+        <div className="skill">
+          <img
+            src="https://image.flaticon.com/icons/png/512/901/901163.png"
+            alt="communication"
+          />
+          <p>COMMUNICATION</p>
+        </div>
+
+        <div className="skill">
+          <img
+            src="https://image.flaticon.com/icons/png/512/3938/3938382.png"
+            alt="coachable"
+          />
+          <p>COACHABLE</p>
+        </div>
+
+        <div className="skill">
+          <img
+            src="https://image.flaticon.com/icons/png/512/3050/3050384.png"
+            alt="problem-solving"
+          />
+          <p>PROBLEM-SOLVING</p>
+        </div>
+
+        <div className="skill">
+          <img
+            src="https://image.flaticon.com/icons/png/512/4752/4752591.png"
+            alt="natural curiosity"
+          />
+          <p>NATURAL CURIOSITY</p>
+        </div>
+      </div>
+
+      <div className="text-container">
+        <div className="brand">
+          <h4>Brand Statement</h4>
+          <br />
+          <p>
+            I am a highly motivated, self-directed and coachable software
+            developer with a unique perspective on the challenges and
+            opportunities organizations face in the digital world. Through my
+            experience in enterprise IT, Security, and Cloud Services, I have
+            proven to be a strong teammate and leader with an aptitude for
+            understanding and solving problems.
+          </p>
+        </div>
+        <div className="tool">
+          <h4>Tools and Technologies</h4>
+          <br />
+          <p>
+            Languages - HTML, CSS, JavaScript, Ruby <br />
+            Frameworks - React, Express, Rails
+            <br />
+            Database - MongoDB, SQL, PostgreSQL, Mongoose
+            <br />
+            Tools - GitHub, CLI, VSCode
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
