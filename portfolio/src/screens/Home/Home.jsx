57e796be97419195229e39633cdd0050a32102f5
Cameron Brilliant <cbrilliant20@gmail.com>
2021-07-12 18:30:06 -0400

    home animations

--- portfolio/src/screens/Home/Home.jsx
+++ portfolio/src/screens/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <h1>
-        Hello, I'm <span>Cameron Brilliant</span>
+        Hello, I'm <span>Cameron Brilliant</span>.
       </h1>
       <h2>Full-Stack Web Developer</h2>
       <button className="welcome-btn btn">Welcome</button>
