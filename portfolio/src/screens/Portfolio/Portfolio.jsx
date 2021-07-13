8af7c963b8e3b8aa245ea5c2b779384c36058ab7
Cameron Brilliant <cbrilliant20@gmail.com>
2021-07-13 18:06:03 -0400

    more css

--- portfolio/src/screens/Portfolio/Portfolio.jsx
+++ portfolio/src/screens/Portfolio/Portfolio.jsx
@@ -6,28 +6,32 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
       <div className="card-container">
         <a href="#" target="_blank" rel="noreferrer">
-          <div className="card">
+          <div className="card card1">
             <h5>Project 1</h5>
             <p>Built with X, Y, and Z</p>
           </div>
         </a>
 
         <a href="#" target="_blank" rel="noreferrer">
-          <div className="card">
+          <div className="card card2">
+            <img
+              src="../../Assets/Screen\ Shot\ 2021-07-13\ at\ 5.59.50\ PM.jpg"
+              alt=""
+            />
             <h5>Project 1</h5>
             <p>Built with X, Y, and Z</p>
           </div>
         </a>
 
         <a href="#" target="_blank" rel="noreferrer">
-          <div className="card">
+          <div className="card card3">
             <h5>Project 1</h5>
             <p>Built with X, Y, and Z</p>
           </div>
         </a>
 
         <a href="#" target="_blank" rel="noreferrer">
-          <div className="card">
+          <div className="card card4">
             <h5>Project 1</h5>
             <p>Built with X, Y, and Z</p>
           </div>
