ec0ec432cc9d9d673ebac98ebeb0eb3a4477ae89
Cameron Brilliant <cbrilliant20@gmail.com>
2021-08-12 11:34:49 -0400

    about page fixed

--- portfolio/src/screens/About/About.jsx
+++ portfolio/src/screens/About/About.jsx
@@ -3,57 +3,42 @@ import thinking from "../../Assets/thinking.png"
 import solve from "../../Assets/solve.png"
 import communicate from "../../Assets/communicate.png"
 import coach from "../../Assets/coach.png"
+import headshot from "../../Assets/headshot.jpg"
 
 const About = () => {
   return (
     <div id="about-container">
       <h2 id="about">About</h2>
-      <div className="skills-container">
-        <div className="skill">
-          <img src={communicate} alt="communication" />
-          <p>COMMUNICATION</p>
-        </div>
-
-        <div className="skill">
-          <img src={coach} alt="coachable" />
-          <p>COACHABLE</p>
-        </div>
-
-        <div className="skill">
-          <img src={solve} alt="problem-solving" />
-          <p>PROBLEM-SOLVING</p>
-        </div>
-
-        <div className="skill">
-          <img src={thinking} alt="natural curiosity" />
-          <p>NATURAL CURIOSITY</p>
-        </div>
-      </div>
 
       <div className="text-container">
-        <div className="brand">
-          <h4>Brand Statement</h4>
-          <br />
-          <p>
-            I am a highly motivated, self-directed and coachable software
-            developer with a unique perspective on the challenges and
-            opportunities organizations face in the digital world. Through my
-            experience in enterprise IT, Security, and Cloud Services, I have
-            proven to be a strong teammate and leader with an aptitude for
-            understanding and solving problems.
-          </p>
-        </div>
+        <img src={headshot} alt="" className="headshot" />
+
         <div className="tool">
-          <h4>Tools and Technologies</h4>
+          <h4>I am a full stack developer based in Boston, Massachusetts.</h4>
           <br />
           <p>
-            Languages - HTML, CSS, JavaScript, Ruby <br />
-            Frameworks - React, Express, Rails
-            <br />
-            Database - MongoDB, SQL, PostgreSQL, Mongoose
-            <br />
-            Tools - GitHub, CLI, VSCode
+            I spent the first five years of my career working at various
+            technology companies in the Infrastructure, Security and Cloud
+            Services segments. Through this exposure to the cutting edge, I have
+            grown a love for software development and creating things of my own.
           </p>
+          <br />
+          <p>
+            Some of the things I've learned along the way:
+            <ul>
+              <li>Lanugages - HTML, CSS, JavaScript, Ruby</li>
+              <li>Frameworks - React, Rails, Express, Mongoose</li>
+              <li>
+                Tools - Github, CLI, Netlify, MongoDB, Chrome Dev Tools, JWT
+              </li>
+            </ul>
+          </p>
+          <br />
+          <p>
+            In my free time you can find me traveling, spending time outdoors,
+            playing sports, and walking my Weimaraner, Billy
+          </p>
+          <br />
           <a
             href="https://drive.google.com/file/d/1zoiL5Yb4vIYhzyxiqpL-CjFKSxyzPcoS/view?usp=sharing"
             target="_blank"
